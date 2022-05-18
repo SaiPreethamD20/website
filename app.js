ecc05e34e61cd4877b0e2bb540f24858529a35b5
Pavan Porendla <89634035+Porendlapavan@users.noreply.github.com>
2022-05-18 14:16:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 /*accesing sidebar*/
+function classToggle() {
+	const navs = document.querySelectorAll('.navbar-links')
 
-var buttone = document.getElementById('theme');
-var sidebar = document.getElementById('sidebar');
+	navs.forEach(nav => nav.classList.toggle('navbar-links-active'));
+}
 
-buttone.addEventListener('click',function()
-{
-	if(sidebar.classList.contains('hide'))
-	{
-		sidebar.classList.add('show');
-		sidebar.classList.remove('hide');
-	}
-	else
-	{
-		sidebar.classList.add('hide');
-		sidebar.classList.remove('show');
-	}
-});
-// 
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl)
-})
+document.querySelector('.toggle-buttons')
+	.addEventListener('click', classToggle);
