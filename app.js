07d06ecef42a9be22872350b249ca7b9264df99b
Pavan Porendla <89634035+Porendlapavan@users.noreply.github.com>
2022-05-20 21:50:29 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/*accesing sidebar*/
-function classToggle() {
-	const navs = document.querySelectorAll('.navbar-links')
-
-	navs.forEach(nav => nav.classList.toggle('navbar-links-active'));
-}
-
-document.querySelector('.toggle-buttons')
-	.addEventListener('click', classToggle);
